34f13585c11415a289257f1fa1809ac2d524f084
Erkam <erkamuzuncayir@gmail.com>
2022-02-06 20:11:59 +0300

    Creating repository and submit first two solutions.

---
+++ introduction_inputAndOutput.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a, b, c, result;
+
+int main()
+{
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    result = a + b + c;
+
+    cout << result << endl;
+    return 0;
+}
